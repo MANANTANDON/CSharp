66bdb6e885fbec61e0dbec6576ae8f0aebd5a5b5
mananTandon <manantandon@gmail.com>
2023-01-20 13:13:58 +0530

    Create testFlight.cs

---
+++ testFlight.cs
@@ -0,0 +1,40 @@
+using System;
+
+
+class Manan
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome To Manan's Land.....\n\n\n");
+        Factorial();
+
+    }
+
+
+    public static void Factorial()
+    {
+        Console.WriteLine("**********FACTORIAL OF A NUMBER**********");
+        string userInput = " ";
+        do
+        {
+            Console.WriteLine("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+            int factorial = 1;
+            for (int i = num; i > 0; i--)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine($"factorial of {num} is : {factorial}");
+            do
+            {
+                Console.WriteLine("Do you go again? : Yes - No :- ");
+                userInput = Console.ReadLine().ToUpper();
+                if (userInput != "YES" && userInput != "NO")
+                {
+                    Console.WriteLine("Please enter a valid input! ");
+                }
+            } while (userInput != "YES" && userInput != "NO");
+
+            } while (userInput == "YES");
+    }
+}
