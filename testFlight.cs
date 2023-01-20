b5b5b543fcd939ec16ad49b12f568cd3d10f43c4
mananTandon <manantandon@gmail.com>
2023-01-20 20:29:28 +0530

    Update testFlight.cs

--- testFlight.cs
+++ testFlight.cs
@@ -7,6 +7,7 @@ class Program
     {
         Console.WriteLine("Welcome.....\n\n\n");
         Factorial();
+        Method();
 
     }
 
@@ -36,5 +37,23 @@ class Program
             } while (userInput != "YES" && userInput != "NO");
 
             } while (userInput == "YES");
+    }
+    public static void Method()
+    {
+        Console.WriteLine("\n\n\n******************CONVERT TO HEXCODE******************");
+        Console.WriteLine("Ready to convert your name to a Hex code ? Yes or No: ");
+        string? userInput = Console.ReadLine()!.ToUpper();
+        if(userInput == "YES") { 
+            Console.WriteLine("Enter your name: ");
+            string? name = Console.ReadLine();
+            byte[] bytes = Encoding.UTF8.GetBytes(name!); //"!" means that the "name" variable will always have a value.
+            string hexCode = Convert.ToHexString(bytes);
+            string finalCode = hexCode.Replace("20", " ");
+            Console.WriteLine($"String: {name} , hexCode: {finalCode}");
+        }
+        else
+        {
+            Console.WriteLine("Okay, we get you dont want your name to be intersting.. seeya ");
+        }
     }
 }
